88034c3e415801bcfac16268de00827ca1d3c047
John Tore Simonsen <jtsimonsen92@gmail.com>
2019-02-24 23:31:49 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+
+function changeBackground(pictureNumber) {
+  if (pictureNumber === 1) {
+    document.getElementsByClassName('image-container')[0].style.background='url(./resources/images/sushi.jpg)no-repeat center center';
+    document.getElementsByClassName('image-content')[0].style.backgroundColor='rgba(9, 18, 23, 0.6)';
+  }else {
+    document.getElementsByClassName('image-container')[0].style.background='url(./resources/images/thomas-bjornstad.jpg)no-repeat center center';
+    document.getElementsByClassName('image-content')[0].style.backgroundColor='rgba(9, 18, 23, 0.1)';
+  }
+
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[pictureNumber-1].className += " active";
+}
